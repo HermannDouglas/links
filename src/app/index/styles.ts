73c2319388c6046722e08c7779c50162151cc577
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-01-23 20:20:43 -0500

    Feat: Adicionar componente Index e estilos para a aplicação React Native

---
+++ src/app/index/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../styles/colors';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+    },
+    title: {
+        color: "colors.green[900]",
+        fontSize: 22,
+    },
+})
