60746690c061f2d5ee88e02c66a8c5161b02da22
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-01-27 15:13:19 -0500

    Feat: Adicionar lista de links dinâmicos ao componente Index

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,4 +1,4 @@
-import { View, Image, TouchableOpacity } from 'react-native'
+import { View, Image, TouchableOpacity, FlatList } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import { styles } from './styles'
@@ -17,7 +17,21 @@ export default function Index() {
                 </TouchableOpacity>
             </View>
             <Categories />
-            <Link name='Rocketseat' url='https://rocketseat.com.br' onDetails={() => console.log("Clicou")} />
+
+            <FlatList
+                data={["1", "2", "3", "4", "5"]}
+                keyExtractor={(item) => item}
+                renderItem={() => (
+                    <Link
+                        name='Rocketseat'
+                        url='https://rocketseat.com.br/'
+                        onDetails={() => console.log("Clicou")}
+                    />
+                )}
+                style={styles.links}
+                contentContainerStyle={styles.linksContent}
+                showsVerticalScrollIndicator={false}
+            />
         </View >
     )
 }
