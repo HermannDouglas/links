0468bde3b4813249aa91a40b7c9adeb472c354fb
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-01-27 20:19:48 -0500

    Feat: Adicionar componente Button e integrá-lo ao formulário no componente Add

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -5,8 +5,9 @@ import { router } from "expo-router";
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
 
-import { Categories } from "@/components/categories";
 import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+import { Categories } from "@/components/categories";
 
 export default function Add() {
     return (
@@ -24,6 +25,7 @@ export default function Add() {
             <View style={styles.form}>
                 <Input placeholder="Nome" />
                 <Input placeholder="Url" />
+                <Button title="Adicionar" />
             </View>
         </View>
     )
